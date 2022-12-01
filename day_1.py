1afc0fc1171aa255f5aa7a8966b5a8d67703bc20
Morgan Waldron <morgan.waldron@versent.com.au>
2022-12-01 16:19:52 +1100

    Day 1, part 1

---
+++ day_1.py
@@ -0,0 +1,33 @@
+from utils import *
+
+
+def part_1(p_Input):
+    elves = [sum(map(int, x.split('\n'))) for x in p_Input.strip().split('\n\n')]
+    return max(elves)
+
+
+def part_2(p_Input):
+    pass
+
+
+example_input_1 = '''1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000'''
+challenge_input = Input('1')
+
+assert(part_1(example_input_1) == 24000)
+print(f"Part 1: {part_1(challenge_input)}")
+
+assert(part_2(example_input_1) == None)
+print(f"Part 2: {part_2(challenge_input)}")
