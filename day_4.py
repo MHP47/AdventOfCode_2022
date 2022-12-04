65300c44ed1251086fedfe07a55d5af275bc08f6
Morgan Waldron <morgan.waldron@versent.com.au>
2022-12-04 19:20:08 +1100

    Day 4, part 2

--- day_4.py
+++ day_4.py
@@ -13,7 +13,16 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    data = p_Input.strip().splitlines()
+    count = 0
+    for pairing in data:
+        elf1_start, elf1_end, elf2_start, elf2_end = map(int, re.findall(r'\d+', pairing))
+        if elf2_start in range(elf1_start, elf1_end + 1) or \
+            elf1_end in range(elf2_start, elf2_end + 1) or \
+            elf1_start in range(elf2_start, elf2_end + 1) or \
+            elf2_end in range(elf1_start, elf1_end + 1):
+            count += 1
+    return count
 
 
 example_input_1 = """2-4,6-8
@@ -27,5 +36,5 @@ challenge_input = Input('4')
 assert(part_1(example_input_1) == 2)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 4)
 print(f"Part 2: {part_2(challenge_input)}")
