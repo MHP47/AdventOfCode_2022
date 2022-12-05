4ee8764f1e18b2713a1b28d6b8945d6bee9b2746
Morgan Waldron <morgan.waldron@versent.com.au>
2022-12-05 17:03:03 +1100

    Day 5, part 2

--- day_5.py
+++ day_5.py
@@ -19,7 +19,20 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    starting_pos, steps = p_Input.split('\n\n')
+    starting_pos = starting_pos.splitlines()
+    stacks = parse_ints(starting_pos[-1])
+    positions = [starting_pos[-1].index(x) for x in map(str, stacks)]
+    crates = [None]# + [deque() for _ in range(len(stacks))]
+    for i in positions:
+        crates.append(deque([x[i] for x in starting_pos[:-1] if x[i] != ' ']))
+
+    for step in steps.splitlines():
+        amount, crate_from, crate_to = parse_ints(step)
+        for i in range(amount):
+            crates[crate_to].insert(i, crates[crate_from].popleft())
+
+    return cat(x[0] for x in crates[1:])
 
 
 example_input_1 = """    [D]    
@@ -37,5 +50,5 @@ challenge_input = Input('5')
 assert(part_1(example_input_1) == 'CMZ')
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 'MCD')
 print(f"Part 2: {part_2(challenge_input)}")
