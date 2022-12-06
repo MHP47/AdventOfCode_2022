9122b5400f1fc335bb837ca353a0f3437d6d60f0
Morgan Waldron <morgan.waldron@versent.com.au>
2022-12-06 16:23:52 +1100

    Day 6, part 2

--- day_6.py
+++ day_6.py
@@ -8,7 +8,9 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    data = p_Input.strip()
+    pos, marker = [(14+i,data[i:i+14]) for i in range(0, len(data)-14) if len(set(data[i:i+14]))==14][0]
+    return pos
 
 
 example_input_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
@@ -25,5 +27,9 @@ assert(part_1(example_input_4) == 10)
 assert(part_1(example_input_5) == 11)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 19)
+assert(part_2(example_input_2) == 23)
+assert(part_2(example_input_3) == 23)
+assert(part_2(example_input_4) == 29)
+assert(part_2(example_input_5) == 26)
 print(f"Part 2: {part_2(challenge_input)}")
