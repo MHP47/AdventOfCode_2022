d4a0a7aaa8913a47ad87ec7da7f6abe1fb211a73
Morgan Waldron <morgan.waldron@versent.com.au>
2022-12-20 20:32:20 +1100

    Day 20, part 2

--- day_20.py
+++ day_20.py
@@ -26,7 +26,44 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    decryption_key = 811589153
+    class Num:
+        def __init__(self, val, prev=None, nxt=None):
+            self.value = val
+            self.prev = prev
+            self.nxt = nxt
+        def __repr__(self):
+            return str(self.value)
+
+    Q = [Num(x * decryption_key) for x in map(int, p_Input.strip().splitlines())]
+    for p,n in zip(Q, Q[1:]):
+        p.nxt = n
+        n.prev = p
+
+    Q[-1].nxt = Q[0]
+    Q[0].prev = Q[-1]
+
+    for _ in range(10):
+        for i in Q:
+            i.prev.nxt = i.nxt
+            i.nxt.prev = i.prev
+            n, p = i.nxt, i.prev
+            for _ in range(i.value%(len(Q)-1)):
+                n = n.nxt
+                p = p.nxt
+            p.nxt = i
+            i.prev = p
+            n.prev = i
+            i.nxt = n
+
+    zero_index = p_Input.strip().splitlines().index('0')
+    total = 0
+    n = Q[zero_index]
+    for _ in range(3):
+        for _ in range(1000):
+            n = n.nxt
+        total += n.value
+    return total
 
 
 example_input_1 = """1
@@ -42,5 +79,5 @@ challenge_input = Input('20')
 assert(part_1(example_input_1) == 3)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 1623178306)
 print(f"Part 2: {part_2(challenge_input)}")
