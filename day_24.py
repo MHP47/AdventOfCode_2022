4fe37f07bd2b2baa084377ce8a6036c023187b8e
Morgan Waldron <morgan.waldron@versent.com.au>
2022-12-29 21:47:24 +1100

    Day 24 speed increase
    Massive improvement on day 24 by not doing dumb shit

--- day_24.py
+++ day_24.py
@@ -1,131 +1,103 @@
 from utils import *
-from copy import deepcopy
-import functools
-
-
-N4 = ((0, 1), (1, 0), (0, -1), (-1, 0))
-
-class Grid(dict):
-    """A 2D grid, implemented as a mapping of {(x, y): cell_contents}."""
-    def __init__(self, mapping=(), rows=(), neighbors=N4):
-        """Initialize with, e.g., either `mapping={(0, 0): 1, (1, 0): 2, ...}`,
-        or `rows=[(1, 2, 3), (4, 5, 6)].
-        `neighbors` is a collection of (dx, dy) deltas to neighboring points.`"""
-        self.update(mapping if mapping else
-            {(x, y): val
-             for y, row in enumerate(rows)
-             for x, val in enumerate(row)})
-        self.width  = max(x for x, y in self) + 1
-        self.height = max(y for x, y in self) + 1
-        self.deltas = neighbors
-        self.blizzards = { i: ((x,y),v) for i,((x,y),v) in enumerate([(k,v) for k,v in self.items() if v in ('<','>','^','v')]) }
-    def copy(self): return Grid(self, neighbors=self.deltas)
-    def neighbors(self, point):
-        """Points on the grid that neighbor `point`."""
-        x, y = point
-        return [(x+dx, y+dy) for (dx, dy) in self.deltas
-            if (x+dx, y+dy) in self and self[(x+dx, y+dy)] != '#'] + [point]
-    def to_rows(self):
-        """The contents of the grid in a rectangular list of lists."""
-        return [[self[x, y] for x in range(self.width)]
-            for y in range(self.height)]
-    def draw(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                print(self.get((x,y), ' '), end='')
-            print()
-        print()
+from operator import sub
 
 
 def part_1(p_Input):
-    grid = Grid(rows=p_Input.strip().splitlines())
+    grid = {(x, y): val for y, row in enumerate(p_Input.strip().splitlines()) for x, val in enumerate(row)}
     start = [(x,y) for (x,y),v in grid.items() if v == '.' and y == 0][0]
-    goal =  [(x,y) for (x,y),v in grid.items() if v == '.' and y == grid.height-1][0]
+    goal =  [(x,y) for (x,y),v in grid.items() if v == '.' and y == max(y for x, y in grid)][0]
+    grid[(start[0], start[1]-1)] = '#'
+    grid[(goal[0], goal[1]+1)] = '#'
     walls = set(k for k,v in grid.items() if v == '#')
-    blizzards = {k:v for k,v in grid.items() if v in ('<','>','^','v')}
-    lookup = {
-        '<': (-1, 0),
-        '>': (1, 0),
-        '^': (0, -1),
-        'v': (0, 1),
-    }
-    grid.update({ k: '.' for k in blizzards })
-    state = {start}
-    seen = {frozenset(blizzards)}
-    t = 0
 
-    def get_blizz(time):
-        r = set()
-        for k,v in blizzards.items():
-            for _ in range(time):
-                k = tuple(sum(x) for x in zip(k, lookup[v]))
-                if k in walls:
-                    if v == '<':
-                        k = (grid.width-2, k[1])
-                    elif v == '>':
-                        k = (1, k[1])
-                    elif v == '^':
-                        k = (k[0], grid.height-2)
-                    elif v == 'v':
-                        k = (k[0], 1)
-            r.add(k)
-        return r
+    class Blizzard:
+        lookup = {
+            '<': (-1, 0),
+            '>': (1, 0),
+            '^': (0, -1),
+            'v': (0, 1),
+        }
+        def __init__(self, start_pos, dir) -> None:
+            self.dir = self.lookup[dir]
+            self.cycle = [start_pos]
+            while True:
+                start_pos = tuple(sum(x) for x in zip(start_pos, self.dir))
+                if start_pos in walls: break
+                self.cycle.append(start_pos)
+            while True:
+                start_pos = tuple(sub(*x) for x in zip(start_pos, self.dir))
+                if start_pos in walls: break
+            while True:
+                start_pos = tuple(sum(x) for x in zip(start_pos, self.dir))
+                if start_pos in self.cycle: break
+                self.cycle.append(start_pos)
+        def new_pos(self):
+            for i in itertools.islice(itertools.cycle(self.cycle), 1, None):
+                yield i
+
+    blizzards = [Blizzard(k,v).new_pos() for k,v in grid.items() if v in ('<','>','^','v')]
+    state = {start}
+    t = 0
 
     while goal not in state:
         t += 1
-        b = set(get_blizz(t))
+        b = set(next(x) for x in blizzards)
         n = set()
         for s in state:
-            n |= set(grid.neighbors(s)) - b
+            n.update(set(neighbors4(s)) - walls - b)
+            if s not in b:
+                n.add(s)
         state = n
 
     return(t)
 
 
 def part_2(p_Input):
-    grid = Grid(rows=p_Input.strip().splitlines())
+    grid = {(x, y): val for y, row in enumerate(p_Input.strip().splitlines()) for x, val in enumerate(row)}
     start = [(x,y) for (x,y),v in grid.items() if v == '.' and y == 0][0]
-    goal =  [(x,y) for (x,y),v in grid.items() if v == '.' and y == grid.height-1][0]
+    goal =  [(x,y) for (x,y),v in grid.items() if v == '.' and y == max(y for x, y in grid)][0]
+    grid[(start[0], start[1]-1)] = '#'
+    grid[(goal[0], goal[1]+1)] = '#'
     walls = set(k for k,v in grid.items() if v == '#')
-    blizzards = {k:v for k,v in grid.items() if v in ('<','>','^','v')}
-    lookup = {
-        '<': (-1, 0),
-        '>': (1, 0),
-        '^': (0, -1),
-        'v': (0, 1),
-    }
-    grid.update({ k: '.' for k in blizzards })
-    seen = {frozenset(blizzards)}
+
+    class Blizzard:
+        lookup = {
+            '<': (-1, 0),
+            '>': (1, 0),
+            '^': (0, -1),
+            'v': (0, 1),
+        }
+        def __init__(self, start_pos, dir) -> None:
+            self.dir = self.lookup[dir]
+            self.cycle = [start_pos]
+            while True:
+                start_pos = tuple(sum(x) for x in zip(start_pos, self.dir))
+                if start_pos in walls: break
+                self.cycle.append(start_pos)
+            while True:
+                start_pos = tuple(sub(*x) for x in zip(start_pos, self.dir))
+                if start_pos in walls: break
+            while True:
+                start_pos = tuple(sum(x) for x in zip(start_pos, self.dir))
+                if start_pos in self.cycle: break
+                self.cycle.append(start_pos)
+        def new_pos(self):
+            for i in itertools.islice(itertools.cycle(self.cycle), 1, None):
+                yield i
+
+    blizzards = [Blizzard(k,v).new_pos() for k,v in grid.items() if v in ('<','>','^','v')]
     t = 0
-
-    @functools.lru_cache(maxsize=None)
-    def get_blizz(time):
-        r = set()
-        for k,v in blizzards.items():
-            for _ in range(time):
-                k = tuple(sum(x) for x in zip(k, lookup[v]))
-                if k in walls:
-                    if v == '<':
-                        k = (grid.width-2, k[1])
-                    elif v == '>':
-                        k = (1, k[1])
-                    elif v == '^':
-                        k = (k[0], grid.height-2)
-                    elif v == 'v':
-                        k = (k[0], 1)
-            r.add(k)
-        return r
-
-    M = lcm(grid.width-2, grid.height-2)
 
     for _ in range(3):
         state = {start}
         while goal not in state:
             t += 1
-            b = set(get_blizz(t%M))
+            b = set(next(x) for x in blizzards)
             n = set()
             for s in state:
-                n |= set(grid.neighbors(s)) - b
+                n.update(set(neighbors4(s)) - walls - b)
+                if s not in b:
+                    n.add(s)
             state = n
         start, goal = goal, start
 
